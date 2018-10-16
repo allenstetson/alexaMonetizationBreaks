c7411700164c8737a12d7299a5c4b837c5033f8f
allen <allen.stetson@gmail.com>
2018-10-15 23:04:04 -0700

    Altered the code to be an exact copy of that found in Amazon's documentation in an attempt to prove that the monetization service does not function as advertized.

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # alexaMonetizationBreaks
 A sample program for Amazon demonstrating the failure of the monetization service
 
-This is a fictional skill called "Camp List" which does absolutely nothing except start up and break Amazon's monetization service in the manner I have observed with all of my skills to date.
-
+This is a fictional skill called "Pink Unicorns" which does absolutely nothing except start up and break Amazon's monetization service in the manner I have observed with all of my skills to date.
+The code is copied straight out of https://alexa-skills-kit-python-sdk.readthedocs.io/en/latest/SERVICE_CLIENTS.html#in-skill-purchase-service and theoretically should work, although - as you'll find - does not.
 ## How I created this
 
 1. I used _developer.amazon.com/alexa/console/ask_ to create a new skill
-2. I set up the interaction model to include the required built-in intents such as _AMAZON.HelpIntent_, and added one custom intent called _ShopIntent_ with invocation prompts including "shop".
+2. I set up the interaction model to include the required built-in intents such as _AMAZON.HelpIntent_, and added monetization intents _BuyProductIntent_ and _CanelProductIntent_.
 3. I used the AWS Console to launch Lambda and create a new Lambda in **Python 3.6**, **us-east-1**, set the Handler to _lambda_function.handler_, added _Alexa Skills Kit_ as a trigger, and attached my custom IAM policy which includes full permissions on CloudWatch, CloudFront, Lambda, DynamoBD, EC2, Certificate Manager, Route 53, S3, IAM, and a slew of other AWS services (total overkill! but effective).
 4. I associated this lambda with my skill via the Endpoint portion of the alexa developer console. _arn:aws:lambda:us-east-1:..._
 5. On my local Windows 10 machine (which has ASK CLI installed already) I used ASK CLI to _ask clone_ my skill
@@ -15,9 +15,9 @@ This is a fictional skill called "Camp List" which does absolutely nothing excep
 8. I pulled relevant packages out of site-packages/Lib/ and put them in the lambda/us-east-1_camplist/ -- this git repo contains all of these packages.
 9. I added an isp to my skill by executing _ask add isp_
 10. I altered the .json file for my isp to be compliant; this included copying the contents of an alexa sample found in github and then adding things such as valid image paths, etc. Again, this file can be found in this repo.
-11. I wrote python in lambda_function.py to use the sdk and SkillBuilder to handle relevant intents, and to make the call to the monetization service as detailed here (notably, the first three lines in the handle() method found in the examples): _https://alexa-skills-kit-python-sdk.readthedocs.io/en/latest/SERVICE_CLIENTS.html#monetizationserviceclient_
+11. I wrote python in lambda_function.py to use the sdk and StandardSkillBuilder to handle relevant intents, and to make the call to the monetization service, copying the code from https://alexa-skills-kit-python-sdk.readthedocs.io/en/latest/SERVICE_CLIENTS.html#in-skill-purchase-service
 12. I performed an _ask deploy_
-13. Using the Test section of the developer console, I tested "Open Camp List" which worked, and then "shop" which throws the familiar 403 error.
+13. Using the Test section of the developer console, I tested "Open Pink Unicorns" which throws the familiar 403 error and responds "There was a problem with the requested skill's response."
 14. I checked my CloudWatch logs for details.
 
 ## Instructions for Replicating the Error
@@ -28,3 +28,66 @@ This is a fictional skill called "Camp List" which does absolutely nothing excep
 4. _ask deploy_ and make sure that it builds correctly.
 5. Follow steps 13 and 14
 
+## CloudWatch Logs
+
+You should see something like the below in your logs, noting that I added statements in the alexa sdk for the Amazon support contact Anand where the failure occurs.
+
+```
+START RequestId: dde2fe66-d106-11e8-bd56-a77369e14463 Version: $LATEST
+*-> ANAND: Now inside ask_sdk_model.services.base_service_client.py
+*-> ANAND: Initializing BaseServiceClient()...
+*-> ANAND: invoke() on BaseServiceClient()...
+*-> ANAND: ApiClientRequest created and populated with the following data:
+*-> ANAND: url: https://api.amazonalexa.com/v1/users/~current/skills/~current/inSkillProducts
+*-> ANAND: method: GET
+*-> ANAND: headers: [('Accept-Language', 'en-US'), ('Content-type', 'application/json'), ('Authorization', 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IjEifQ.eyJhdWQiOiJodHRwczovL2FwaS5hbWF6b25hbGV4YS5jb20iLCJpc3MiOiJBbGV4YVNraWxsS2l0Iiwic3ViIjoiYW16bjEuYXNrLnNraWxsLmRkNWQ3YTUxLTgwN2UtNDZiNC05ZDM1LWIxZGVlMTg1NDIzYSIsImV4cCI6MTUzOTY3MjQwNSwiaWF0IjoxNTM5NjY4ODA1LCJuYmYiOjE1Mzk2Njg4MDUsInByaXZhdGVDbGFpbXMiOnsiY29uc2VudFRva2VuIjpudWxsLCJkZXZpY2VJZCI6ImFtem4xLmFzay5kZXZpY2UuQUdHUkNFQktYR1g3VFNHTUlOSERGTFpDRTY0WVg0QTZRSFU2NFJYTExKSkNJU0lRMkhJU1NUWkxERTVFV0I0WlBJN1hSUVdDTEdMSE1FS09KTFdXQkdCS0lCS0FGVFQ1VUhSQVlIWlNKQTNQVldGQ0xaRDZDRkpDTDIyQkpLNzVaQzVVREpWS002NzU2WDM0UlFLSTdCWUhQNDNRIiwidXNlcklkIjoiYW16bjEuYXNrLmFjY291bnQuQUhHVjRRMlhGVVVKNkxVWTZMN002Q0NXTFJGVTVDR1ZVVzJNMkpGRTJTNkpCSDJOUk9YQ1pUSFM2VFFSUUpFT05HVVlFWVBXT1dRVFU2NVZYTkpaQ1U2N1JYTENSTTM2SjdRNVpQQkpRWEtDWFRIMkZaRk9PWk9XM0czSUtBSlZZRlRCNlJGSlRWR0ZXS0NXR0pFMzdPV1RVR0FRQk41N01ZNTRLSE5QNURMWVBRRUxORjZUWlc0MjRRU0NHV1RMNEFBVFNYSUZCS0FNWVVJIn19.QzTj59CPDj08XZw2I54pG9Zdnk48Vi5JP-2GPd6lJ_nRQE6XL_8y9LmjHIZSPNX8GbpYQcfoTc3FmxibpRxQW4twQtapQvIF6ervhOit52UzQcaf0eJPJKNq9UwZ-oDOQFmAP65ugdyagvg9HtZktyUqp-gDh3EazB6OD06KuZK1v3SgjXB5IQlRr4j1PxomSk1koqGJgysjwVbx1cgrvvFv4fEV3KPLbDEsXISm4xho7jJeSJfEp-sZ-X_Kkkat8ex3z7HJGiq63h9sQrSv-HorCOQHEpn3J_AejvgZKDVeim56fJKpWJG1VH3Myh4o5-C4h7fL1G30bLW4kCZFfg')]
+*-> ANAND: body: None
+*-> ANAND: calling self._api_client.invoke(request)
+*-> ANAND: api client invocation successful; no Exception raised.
+*-> ANAND (!!!): BaseServiceClient.__is_code_successful(response.status_code) returned false.
+*-> ANAND: No response_definitions were passed into this method... raising a ServiceException
+*-> ANAND: ServiceException message: Unknown Error
+*-> ANAND: ServiceException status_code: 403
+*-> ANAND: ServiceException headers: [('Server', 'CloudFront'), ('Date', 'Tue'), ('Date', '16 Oct 2018 05:46:46 GMT'), ('Content-Type', 'text/html'), ('Content-Length', '556'), ('Connection', 'keep-alive'), ('X-Cache', 'Error from cloudfront'), ('Via', '1.1 51c76241371dfc20d25094a51b4759eb.cloudfront.net (CloudFront)'), ('X-Amz-Cf-Id', 'q3OA_ysVzjypuMJtFmeIS1exW9-5FY8NiKJhtvSxsx5RB67zPGj0TQ==')]
+*-> ANAND: ServiceException body: <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<HTML><HEAD><META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=iso-8859-1">
+<TITLE>ERROR: The request could not be satisfied</TITLE>
+</HEAD><BODY>
+<H1>403 ERROR</H1>
+<H2>The request could not be satisfied.</H2>
+<HR noshade size="1px">
+Bad request.
+
+<BR clear="all">
+<HR noshade size="1px">
+<PRE>
+Generated by cloudfront (CloudFront)
+Request ID: q3OA_ysVzjypuMJtFmeIS1exW9-5FY8NiKJhtvSxsx5RB67zPGj0TQ==
+</PRE>
+<ADDRESS>
+</ADDRESS>
+</BODY></HTML>
+Unknown error: ServiceException
+Traceback (most recent call last):
+File "/var/task/ask_sdk_core/skill_builder.py", line 187, in wrapper
+request_envelope=request_envelope, context=context)
+File "/var/task/ask_sdk_core/skill.py", line 191, in invoke
+response = self.request_dispatcher.dispatch(handler_input)
+File "/var/task/ask_sdk_core/dispatch.py", line 164, in dispatch
+raise e
+File "/var/task/ask_sdk_core/dispatch.py", line 149, in dispatch
+response = self.__dispatch_request(handler_input)
+File "/var/task/ask_sdk_core/dispatch.py", line 217, in __dispatch_request
+handler_input=handler_input, handler=request_handler)
+File "/var/task/ask_sdk_core/dispatch_components/request_components.py", line 478, in execute
+return handler.handle(handler_input)
+File "/var/task/lambda_function.py", line 40, in handle
+product_response = ms.get_in_skill_products(locale)
+File "/var/task/ask_sdk_model/services/monetization/monetization_service_client.py", line 120, in get_in_skill_products
+response_type="ask_sdk_model.services.monetization.in_skill_products_response.InSkillProductsResponse")
+File "/var/task/ask_sdk_model/services/base_service_client.py", line 129, in invoke
+message="Unknown error", status_code=response.status_code, headers=response.headers, body=response.body)
+ask_sdk_model.services.service_exception.ServiceException: Unknown error
+
+END RequestId: dde2fe66-d106-11e8-bd56-a77369e14463
+```
--- lambda/us-east-1_camplist/lambda_function.py
+++ lambda/us-east-1_camplist/lambda_function.py
@@ -1,51 +1,139 @@
+# =============================================================================
+# IMPORTS
+# =============================================================================
+import logging
 import random
-
-from ask_sdk_core.dispatch_components import AbstractRequestHandler
 from ask_sdk_core.utils import is_request_type, is_intent_name
 from ask_sdk.standard import StandardSkillBuilder
+from ask_sdk_core.dispatch_components import AbstractRequestHandler
+from ask_sdk_core.handler_input import HandlerInput
+from ask_sdk_core.utils import is_request_type
+from ask_sdk_model.response import Response
+from ask_sdk_model.services.monetization import (
+    EntitledState, PurchasableState, InSkillProductsResponse)
+from ask_sdk_model.interfaces.connections import SendRequestDirective
 
-sb = StandardSkillBuilder()
+
+# =============================================================================
+# LOGGING
+# =============================================================================
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 
+# =============================================================================
+# CLASSES
+# =============================================================================
+
+# -----------------------------------------------------------------------------
+# The following are Monetization handlers copied verbatim (*no lines changed!)
+# from:
+# https://alexa-skills-kit-python-sdk.readthedocs.io/en/latest/SERVICE_CLIENTS.html#in-skill-purchase-service
+# -----------------------------------------------------------------------------
 class LaunchRequestHandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
-        """Inform the request handler of what intents can be handled by this object.
-
-        Args:
-            handler_input (ask_sdk_core.handler_input.HandlerInput): The input from Alexa.
-
-        Returns:
-            bool: Whether or not the current intent can be handled by this object.
-
-        """
         return is_request_type("LaunchRequest")(handler_input)
 
     def handle(self, handler_input):
-        """ Handle the launch request; fetch and serve the appropriate response.
+        locale = handler_input.request_envelope.request.locale
+        ms = handler_input.service_client_factory.get_monetization_service()
+        product_response = ms.get_in_skill_products(locale)
 
-        Args:
-            handler_input (ask_sdk_core.handler_input.HandlerInput): The input from Alexa.
+        if isinstance(product_response, InSkillProductsResponse):
+            total_products = len(product_response.in_skill_products)
+            entitled_products = len([l for l in product_response.in_skill_products
+                                     if l.entitled == EntitledState.ENTITLED])
+            purchasable_products = len([l for l in product_response.in_skill_products
+                                        if l.purchasable == PurchasableState.PURCHASABLE])
 
-        Raises:
-            ValueError: If something other than the sanctioned app calls this intent.
+            speech = (
+                "Found total {} products of which {} are purchasable and {} "
+                "are entitled".format(
+                    total_products, purchasable_products, entitled_products))
+        else:
+            speech = "Something went wrong in loading your purchase history."
 
-        Returns:
-            ask_sdk_model.response.Response: Response for this intent and device.
-
-        """
-        # Prevent someone else from configuring a skill that sends requests to this:
-        if handler_input.request_envelope.session.application.application_id != \
-                "amzn1.ask.skill.dd5d7a51-807e-46b4-9d35-b1dee185423a":
-            raise ValueError("Invalid Application ID")
-
-        speech = "Camp list provides a checklist of items to pack for camping trips. "
-        speech += "Would you like me to help you plan a trip? "
-        reprompt = "What do you want to do? "
-
-        return handler_input.response_builder.speak(speech).ask(
-            reprompt).set_should_end_session(False).response
+        return handler_input.response_builder.speak(speech).response
 
 
+class BuyProductIntentHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_intent_name("BuyProductIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+
+        # Obtain the corresponding product_id for the requested in-skill
+        # product by invoking InSkillProducts API.
+        # The slot variable product_name used below is only for demonstration.
+
+        locale = handler_input.request_envelope.request.locale
+        ms = handler_input.service_client_factory.get_monetization_service()
+
+        product_response = ms.get_in_skill_products(locale)
+        slots = handler_input.request_envelope.request.intent.slots
+        product_ref_name = slots.get("product_name").value
+        product_record = [l for l in product_response.in_skill_products
+                          if l.reference_name == product_ref_name]
+
+        if product_record:
+            return handler_input.response_builder.add_directive(
+                SendRequestDirective(
+                    name="Buy",
+                    payload={
+                        "InSkillProduct": {
+                            "productId": product_record[0].product_id
+                        }
+                    },
+                    token="correlationToken")
+            ).response
+        else:
+            return handler_input.response_builder.speak(
+                "I am sorry. That product is not available for purchase"
+                ).response
+
+
+class CancelProductIntentHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_intent_name("CancelProductIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+
+        # Obtain the corresponding product_id for the requested in-skill
+        # product by invoking InSkillProducts API.
+        # The slot variable product_name used below is only for demonstration.
+
+        locale = handler_input.request_envelope.request.locale
+        ms = handler_input.service_client_factory.get_monetization_service()
+
+        product_response = ms.get_in_skill_products(locale)
+        slots = handler_input.request_envelope.request.intent.slots
+        product_ref_name = slots.get("product_name").value
+        product_record = [l for l in product_response.in_skill_products
+                          if l.reference_name == product_ref_name]
+
+        if product_record:
+            return handler_input.response_builder.add_directive(
+                SendRequestDirective(
+                    name="Cancel",
+                    payload={
+                        "InSkillProduct": {
+                            "productId": product_record[0].product_id
+                        }
+                    },
+                    token="correlationToken")
+            ).response
+        else:
+            return handler_input.response_builder.speak(
+                "I am sorry. I don't know that one").response
+
+
+# -----------------------------------------------------------------------------
+# The following are standard handlers that I wrote to handle built-in intents.
+# -----------------------------------------------------------------------------
 class CancelAndStopIntentHandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
         """Inform the request handler of what intents/requests can be handled by this object.
@@ -120,67 +208,17 @@ class SessionEndedRequestHandler(AbstractRequestHandler):
         responseBuilder.set_should_end_session(True)
         return handler_input.response_builder.response
 
-
-class ProductShopHandler(AbstractRequestHandler):
-    def can_handle(self, handler_input):
-        """Inform the request handler of what intents can be handled by this object.
-
-        Args:
-            handler_input (ask_sdk_core.handler_input.HandlerInput): The input from Alexa.
-
-        Returns:
-            bool: Whether or not the current intent can be handled by this object.
-
-        """
-        return is_intent_name("ShopIntent")(handler_input)
-
-    def handle(self, handler_input):
-        """ Handle the launch request; fetch and serve the appropriate response.
-
-        Args:
-            handler_input (ask_sdk_core.handler_input.HandlerInput): The input from Alexa.
-
-        Returns:
-            ask_sdk_model.response.Response: Response for this intent and device.
-
-        """
-
-        # productName = handler_input.request_envelope.request.intent.slots["ProductName"]["value"]
-        locale = handler_input.request_envelope.request.locale
-        ms = handler_input.service_client_factory.get_monetization_service()
-        inSkillResponse = ms.get_in_skill_products(locale)
-
-        if not inSkillResponse:
-            speech = ("I am having trouble reaching Amazon's monetization "
-                      "service. What else can I do for you?")
-            reprompt = "I didn't catch that. Can you try again?"
-            return handler_input.response_builder.speak(speech).ask(
-                reprompt).response
-
-        # Inform the user about what products are available for purchase
-        purchasable = [l for l in inSkillResponse.in_skill_products
-                       if l.entitled == EntitledState.NOT_ENTITLED and
-                       l.purchasable == PurchasableState.PURCHASABLE]
-
-        if purchasable:
-            speech = ("There is currently one item for sale. ")
-        else:
-            owned = [x for x in inSkillResponse.in_skill_products
-                     if x.entitled == EntitledState.NOT_ENTITLED]
-            if len(owned) == len (list(inSkillResponse.in_skill_products)):
-                speech = ("It appears as if you already own all items "
-                          " What else can I help you with? ")
-            else:
-                speech = ("There are currently no more items "
-                          "available. What can I help you with?")
-        reprompt = "Try saying I'm a butt. "
-        return handler_input.response_builder.speak(speech).ask(
-            reprompt).response
-
-
+# =============================================================================
+# SKILL BUILDER
+# =============================================================================
+sb = StandardSkillBuilder()
+# Monetization:
 sb.add_request_handler(LaunchRequestHandler())
+sb.add_request_handler(BuyProductIntentHandler())
+sb.add_request_handler(CancelProductIntentHandler())
+# Standard:
 sb.add_request_handler(CancelAndStopIntentHandler())
 sb.add_request_handler(SessionEndedRequestHandler())
-sb.add_request_handler(ProductShopHandler())
+
 
 handler = sb.lambda_handler()
